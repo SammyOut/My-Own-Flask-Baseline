2aacc4ca000d1dd23fce2ee57cf434702d329181
RISMME <python@istruly.sexy>
2018-11-14 14:04:04 +0900

    NEW: cors

--- Server/app.py
+++ Server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger
 
@@ -11,6 +12,7 @@ def create_app(config) -> Flask:
     app: Flask = Flask(__name__)
     app.config.from_object(config)
 
+    CORS(app)
     JWTManager(app)
     Swagger(app, template=app.config['SWAGGER_TEMPLATE'])
 
