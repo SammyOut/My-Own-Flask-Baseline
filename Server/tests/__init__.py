e5ed3097c02c9b7dff80661264d63d1487d4f56b
Nerd-Bear <python@istruly.sexy>
2018-11-19 01:28:27 +0900

    UPDATE: Auth Test

--- Server/tests/__init__.py
+++ Server/tests/__init__.py
@@ -1,3 +1,5 @@
+from json import dumps
+
 from pymongo import MongoClient
 from unittest import TestCase
 
@@ -15,3 +17,12 @@ class TCBase(TestCase):
 
     def tearDown(self):
         self.mongo_client.drop_database(TestConfig.DB_NAME)
+
+    def create_account(self, id_='12345', password='12345678', email='python@istruly.sexy'):
+        self.client.post(
+            '/signup',
+            data=dumps(
+                dict(id=id_, password=password, email=email)
+            ),
+            content_type='application/json'
+        )
--- Server/tests/account/auth.py
+++ Server/tests/account/auth.py
@@ -5,10 +5,33 @@ from tests import TCBase
 
 class AuthTest(TCBase):
 
-    def auth_request(self, id='Nerd-Bear', password='Nerd-Bear'):
+    def setUp(self):
+        super(AuthTest, self).setUp()
+        self.create_account()
+
+    def auth_request(self, id='12345', password='12345678'):
         return self.client.post(
             '/auth',
             data=dumps(
                 dict(id=id, password=password)
-            )
+            ),
+            content_type='application/json'
         )
+
+    def _success_signup(self, rv):
+        self.assertEqual(rv.status_code, 200)
+
+    def _fail_signup(self, rv):
+        self.assertEqual(rv.status_code, 204)
+
+    def test_wront_id(self):
+        rv = self.auth_request(id='123456')
+        self._fail_signup(rv)
+
+    def test_wrong_password(self):
+        rv = self.auth_request(password='1234567890')
+        self._fail_signup(rv)
+
+    def test_success(self):
+        rv = self.auth_request()
+        self._success_signup(rv)
